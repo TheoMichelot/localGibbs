a4126aca32418418475cd539d04ef68b7d4e5b93
Theo Michelot <theo.michelot@gmail.com>
2018-06-11 07:59:55 +0100

    add light option to plotRaster

--- R/plotRaster.R
+++ R/plotRaster.R
@@ -6,6 +6,7 @@
 #' @param norm Logical. Should the raster be normalized to integrate to 1?
 #' @param log Logical. Should the raster be plotted on the log scale?
 #' @param name Name of the scale
+#' @param light Logical. If TRUE, the plot is minimalistic.
 #' 
 #' @importFrom ggplot2 ggplot geom_raster coord_equal geom_point geom_path aes_string
 #' @importFrom viridis scale_fill_viridis
@@ -13,7 +14,7 @@
 #' @importFrom sp coordinates
 #' 
 #' @export
-plotRaster <- function(rast, xy=NULL, norm=FALSE, log=FALSE, name="")
+plotRaster <- function(rast, xy=NULL, norm=FALSE, log=FALSE, name="", light=FALSE)
 {
     covmap <- data.frame(coordinates(rast),val=values(rast))
     
@@ -26,12 +27,20 @@ plotRaster <- function(rast, xy=NULL, norm=FALSE, log=FALSE, name="")
     }
     
     p <- ggplot(covmap, aes_string(x="x",y="y")) + geom_raster(aes_string(fill="val")) +
-        coord_equal() + scale_fill_viridis(name=name)
+        coord_equal() 
     
     if(!is.null(xy)) {
         xydf <- data.frame(x=xy[,1], y=xy[,2])
         p <- p + geom_point(aes_string(x="x",y="y"), data=xydf, size=0.3) +
             geom_path(aes_string(x="x",y="y"), data=xydf, size=0.5)
+    }
+    
+    if(light) {
+        p <- p + scale_fill_viridis(guide="none") +
+            theme(axis.title = element_blank(), axis.text = element_blank(), 
+                       axis.ticks = element_blank())
+    } else {
+        p <- p + scale_fill_viridis(name=name)
     }
     
     return(p)
