d1f45454b72374dc48db5c27d6de058171152728
TheoMichelot <theo.michelot@gmail.com>
2018-06-11 13:49:41 +0100

    add error in rsf if index out of bounds

--- src/rsf.cpp
+++ src/rsf.cpp
@@ -12,13 +12,20 @@
 double rsf(arma::rowvec xy, arma::vec beta, arma::cube& cov, arma::vec lim, arma::vec res)
 {
     // return 0 if outside region
-    if(xy(0)<lim(0) || xy(0)>lim(1) || xy(1)<lim(2)  || xy(1)>lim(3))
+    if(xy(0)<=lim(0) || xy(0)>=lim(1) || xy(1)<=lim(2)  || xy(1)>=lim(3))
         return 0;
     
     // extract covariate values
     int i = floor((lim(3)-xy(1))/res(1));
     int j = floor((xy(0)-lim(0))/res(0));
-    arma::vec covxy = cov.tube(i,j);
+    
+    arma::vec covxy(size(cov)(2));
+    
+    if(i>=size(cov)(0) || j>=size(cov)(1)) {
+        Rcpp::stop("Error in rsf: index out of bounds. Check lim and res.");
+    } else {
+        covxy = cov.tube(i,j);
+    }
     
     // return 0 if NA covariate values (covariate unmapped)
     for(int k=0; k<covxy.size(); k++) {
