1f7aa269a40e7d52b0c8574ea42168f8c135762d
TheoMichelot <theo.michelot@gmail.com>
2018-06-14 17:48:58 +0100

    update NAMESPACE and init file

--- src/localGibbs_init.c
+++ src/localGibbs_init.c
@@ -6,6 +6,7 @@
 // Generated with tools::package_native_routine_registration_skeleton
 
 /* .Call calls */
+extern SEXP _localGibbs_nllkLG_norm_rcpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP _localGibbs_nllkLG_rcpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP _localGibbs_rsf(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP _localGibbs_rsfvec(SEXP, SEXP, SEXP, SEXP, SEXP);
@@ -15,13 +16,14 @@ extern SEXP _localGibbs_simSSF_rcpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SE
 extern SEXP _localGibbs_simZeros_rcpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_localGibbs_nllkLG_rcpp",   (DL_FUNC) &_localGibbs_nllkLG_rcpp,   12},
-    {"_localGibbs_rsf",           (DL_FUNC) &_localGibbs_rsf,            5},
-    {"_localGibbs_rsfvec",        (DL_FUNC) &_localGibbs_rsfvec,         5},
-    {"_localGibbs_scalez",        (DL_FUNC) &_localGibbs_scalez,         5},
-    {"_localGibbs_simLG_rcpp",    (DL_FUNC) &_localGibbs_simLG_rcpp,     7},
-    {"_localGibbs_simSSF_rcpp",   (DL_FUNC) &_localGibbs_simSSF_rcpp,   12},
-    {"_localGibbs_simZeros_rcpp", (DL_FUNC) &_localGibbs_simZeros_rcpp,  6},
+    {"_localGibbs_nllkLG_norm_rcpp", (DL_FUNC) &_localGibbs_nllkLG_norm_rcpp,  9},
+    {"_localGibbs_nllkLG_rcpp",      (DL_FUNC) &_localGibbs_nllkLG_rcpp,      12},
+    {"_localGibbs_rsf",              (DL_FUNC) &_localGibbs_rsf,               5},
+    {"_localGibbs_rsfvec",           (DL_FUNC) &_localGibbs_rsfvec,            5},
+    {"_localGibbs_scalez",           (DL_FUNC) &_localGibbs_scalez,            5},
+    {"_localGibbs_simLG_rcpp",       (DL_FUNC) &_localGibbs_simLG_rcpp,        7},
+    {"_localGibbs_simSSF_rcpp",      (DL_FUNC) &_localGibbs_simSSF_rcpp,      12},
+    {"_localGibbs_simZeros_rcpp",    (DL_FUNC) &_localGibbs_simZeros_rcpp,     6},
     {NULL, NULL, 0}
 };
 
