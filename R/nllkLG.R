64b24f934d0e671bf0d5c306bd3553b0f3d7043c
TheoMichelot <theo.michelot@gmail.com>
2018-06-16 18:06:49 +0100

    remove nllkLG_debug separate function

--- R/nllkLG.R
+++ R/nllkLG.R
@@ -11,53 +11,14 @@
 #' @param cov Array of covariates (one layer for each covariate)
 #' @param lim Limits of the covariate rasters.
 #' @param res Resolution of the covariate rasters.
+#' @param debug Logical. If TRUE, the function also returns the log-likelihood
+#' of each step
 #' 
 #' @export
 #' 
 #' @useDynLib localGibbs
-nllkLG <- function(par, ID=NULL, xy, rdist=c("fixed", "exp", "gamma"), MCgrids, cov, lim, res)
-{
-    # consider unique track if ID==NULL
-    if(is.null(ID))
-        ID <- rep(1,nrow(xy))
-    
-    gridc <- MCgrids$gridc
-    gridz <- MCgrids$gridz
-    
-    # unpack parameters
-    tpar <- w2n(wpar=par, rdist=rdist, xy=xy)
-    beta <- tpar$beta
-    r <- tpar$r
-    shape <- tpar$shape
-    rate <- tpar$rate
-    
-    if(length(beta)!=dim(cov)[3])
-        stop("Wrong number of parameters provided")
-    
-    if(rdist=="fixed") {
-        truncr <- matrix(r, nrow=nrow(xy)-1, ncol=1)
-    } else {
-        gridr <- MCgrids$gridr
-        truncr <- truncgridr(shape, rate, ID, xy, gridr)
-    }
-    
-    # Rcpp doesn't cope with NULL
-    if(is.null(shape))
-        shape <- NA
-    if(is.null(rate))
-        rate <- NA
-    
-    # compute negative log-likelihood in C++
-    nllk <- nllkLG_rcpp(beta=beta, shape=shape, rate=rate, ID=ID, xy=xy, rdist=rdist,
-                        truncr=truncr, gridc=gridc, gridz=gridz, cov=cov, lim=lim, res=res)$nllk
-    
-    return(nllk)
-}
-
-
-#' Negative log-likelihood function for the local Gibbs model (debug version)
-#' @export
-nllkLG_debug <- function(par, ID=NULL, xy, rdist=c("fixed", "exp", "gamma"), MCgrids, cov, lim, res)
+nllkLG <- function(par, ID=NULL, xy, rdist=c("fixed", "exp", "gamma"), MCgrids, 
+                   cov, lim, res, debug=FALSE)
 {
     # consider unique track if ID==NULL
     if(is.null(ID))
@@ -93,5 +54,7 @@ nllkLG_debug <- function(par, ID=NULL, xy, rdist=c("fixed", "exp", "gamma"), MCg
     nllk <- nllkLG_rcpp(beta=beta, shape=shape, rate=rate, ID=ID, xy=xy, rdist=rdist,
                         truncr=truncr, gridc=gridc, gridz=gridz, cov=cov, lim=lim, res=res)
     
+    if(!debug)
+        nllk <- nllk$nllk
     return(nllk)
 }
